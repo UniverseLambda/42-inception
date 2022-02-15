6d12c83414b481b79a81591d69e7af397b9182da
clsaad <clsaad@student.42lyon.fr>
2022-02-15 13:39:54 +0100

    Final commit?
    add restart policy
    modify Wordpress admin password (because the moron I am forgot the password)
    fix Wordpress could not upload media

--- srcs/containers/wordpress/data/start.sh
+++ srcs/containers/wordpress/data/start.sh
@@ -5,6 +5,8 @@ if [[ ! -d /var/www/html/wordpress ]]; then
     mkdir -p /var/www/html/wordpress
     unzip /clsaad_container_data/latest.zip -d /var/www/html
     cp /clsaad_container_data/wp-config.php /var/www/html/wordpress
+    mkdir -p /var/www/html/wordpress/wp-content
+    chmod -R 777 /var/www/html/wordpress/wp-content
 fi
 
 php-fpm8 -F -R
