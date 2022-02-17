ae18cfbc3fb2164580260ab3113b6a1a9c9c05fb
SAAD Clement <44324085+UniverseLambda@users.noreply.github.com>
2022-02-17 12:38:05 +0100

    WELL WELL WELL. Fuck you correction
    add a user account to Wordpress (Mamaaaaaaaa)
    
    fix mariadb not asking password when connecting to root as sudo
    fix root has no password
    fix every alpine linux version (penultimate)
    
    remove suprem_commander mariadb user

--- srcs/containers/mariadb/data/start.sh
+++ srcs/containers/mariadb/data/start.sh
@@ -9,13 +9,15 @@ if [[ ! -d /var/lib/mysql/LuffyForEver_Wordpress ]]; then
     mysql -u root -e "CREATE DATABASE LuffyForEver_Wordpress;
     CREATE USER '$USER_NAME' IDENTIFIED BY '$USER_PASSWORD';
     GRANT ALL PRIVILEGES ON LuffyForEver_Wordpress.* TO '$USER_NAME'@'%';
-    CREATE USER 'suprem_commander'@'%' IDENTIFIED BY '$ADMIN_PASSWORD';
-    GRANT ALL PRIVILEGES ON *.* TO 'suprem_commander'@'%' WITH GRANT OPTION;
+    UPDATE mysql.user SET plugin = '' WHERE user = 'root' AND host = 'localhost';
     FLUSH PRIVILEGES;"
 
     mysql -u root LuffyForEver_Wordpress < /clsaad_container_data/wp-db.mysql
+    
+    mysqladmin -u root password "$ROOT_PASSWORD"
 
     service mysql stop
+
 fi
 
 /bin/sh /usr/bin/mysqld_safe
